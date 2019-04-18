52f9ed4940ce9dc46edf16839661653c3d2dbac1
Sophie Mori <ksmori14@gmail.com>
2019-04-18 00:30:13 -0400

    Recent updates

--- js/projects.js
+++ js/projects.js
@@ -7,26 +7,6 @@ const l = {
   PUZ: 6,
   LAN: 7,
 };
-const projects = [
-  makeProject("Aquarium puzzlehunt", "aqua", "",
-  "Writing puzzles for freshmen orientation event at the aquarium",
-  [l.PUZ], true),
-];
-  AQUA: {rank: 1, name: "Aquarium puzzlehunt", labels: [], public: true},
-  BASIS: {rank: 1, name: "Basis Tech", labels: [], public: false},
-  BOAT: {rank: 1, name: "Electric motor boat", labels: [], public: true},
-  BUD: {rank: 1, name: "bud:2020", labels: [], public: false},
-  CONLANG: {rank: 1, name: "Kirha: a constructed language", labels: [], public: false},
-  CR2: {rank: 1, name: "Courseroad v2", labels: [], public: false},
-  ESL: {rank: 1, name: "ESL treebank", labels: [], public: false},
-  ESP: {rank: 1, name: "ESPider", labels: [], public: false},
-  FORCES: {rank: 1, name: "Forces frozen", labels: [], public: true},
-  GTTA: {rank: 1, name: "Gotta Track 'Em All", labels: [], public: false},
-  HAUNT: {rank: 1, name: "Next haunt", labels: [], public: true},
-  IPAFUZZ: {rank: 1, name: "Fuzzy matching IPA", labels: [], public: false},
-  PAPER: {rank: 1, name: "Paper structures", labels: [], public: false},
-  WB: {rank: 1, name: "Wonder Blocks", labels: [], public: false},
-};
 
 const makeProject = function(name, tag, src, desc, labels, public, rank) {
   return {
